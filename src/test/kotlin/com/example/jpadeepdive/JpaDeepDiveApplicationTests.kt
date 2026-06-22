c2177e9b4f4fe6c24736494827e48b1f8aaa89d7
wldks1008 <gayeon1008@g.skku.edu>
2026-06-22 21:44:42 +0900

    init: 초기 세팅

---
+++ src/test/kotlin/com/example/jpadeepdive/JpaDeepDiveApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.example.jpadeepdive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JpaDeepDiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
